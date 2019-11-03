37a06e05bb310d09d3ae014709234a059b3c48e1
Melike KILIC <melike180400@gmail.com>
2019-11-03 01:59:16 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -3,7 +3,12 @@ require "yaml"# require modules here
 
 def load_library(file_path)
   emoticons = {"get_meaning" => {}, "get_emoticon" => {}}
-   YAML.load_file(file_path)
+   YAML.load_file.each { |key,value|
+     english,japan= value
+     emoticons["get_meaning"][japan]= key
+     emoticons["get_emoticon"][english]= japan
+     
+   }
   emoticons
 end
   # code goes here
