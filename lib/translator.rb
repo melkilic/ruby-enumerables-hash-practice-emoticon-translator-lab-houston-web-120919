7d6c72dce62460a2ad613b0343b2d162547df5fa
Melike KILIC <melike180400@gmail.com>
2019-11-03 01:31:35 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,7 +4,7 @@ require "yaml"# require modules here
 def load_library(file_path)
   emoticons= YAML.load_file(file_path)
   emoticons << ':get_meaning' && ':get_emoticon'
-  emoticon
+  emoticons
   # code goes here
 end
 
