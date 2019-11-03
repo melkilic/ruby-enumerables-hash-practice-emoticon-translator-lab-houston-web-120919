187f4fc447d7cc267fedcc325bb27b56c6192a80
Melike KILIC <melike180400@gmail.com>
2019-11-03 01:08:40 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -2,46 +2,7 @@
 require "yaml"# require modules here
 
 def load_library(emoticons)
-  emoticons=angel:
-    - "O:)"
-    - "☜(⌒▽⌒)☞"
-  angry:
-    - ">:("
-    - "ヽ(ｏ`皿′ｏ)ﾉ"
-  bored:
-    - ":O"
-    - "(ΘεΘ;)"
-  confused:
-    - "%)"
-    - "(゜.゜)"
-  embarrased:
-    - ":$"
-    - "(#^.^#)"
-  fish:
-    - "><>"
-    - ">゜))))彡"
-  glasses:
-    - "8D"
-    - "(^0_0^)"
-  grinning:
-    - "=D"
-    - "(￣ー￣)"
-  happy:
-    - ":)"
-    - "(＾ｖ＾)"
-  kiss:
-    - ":*"
-    - "(*^3^)/~☆"
-  sad:
-    - ":'("
-    - "(Ｔ▽Ｔ)"
-  surprised:
-    - ":o"
-    - "o_O"
-  wink:
-    - ";)"
-    - "(^_-)"
-  emoticons= YAML.load_file('emoticons.yml')
+   emoticons= YAML.load_file('emoticons.yml')
   emoticons
   # code goes here
 end
